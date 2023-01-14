430991838ecddebd95c7e55ff1ce520ffa2e780a
umar <umar.osamafarooq@gmail.com>
2023-01-14 13:07:41 +0500

    updates

--- src/components/DashboardNav.js
+++ src/components/DashboardNav.js
@@ -3,37 +3,41 @@ import dIcon from '../assets/dashboardIcon.svg'
 import alertIcon from '../assets/alertIcon.svg'
 import defendIcon from '../assets/defenderIcon.svg'
 import protectIcon from '../assets/protectIcon.svg'
+import { Link } from 'react-router-dom'
 
 const DashboardNav = () => {
     // const dashboardItems = ['Dashboard', 'Alerts', 'Defend', 'Protect'];
     // const dashboardIcons = [dIcon, alertIcon, defendIcon, protectIcon];
-
+    const items = [
+        { id: 1, item: 'Dashboard', Icon: dIcon, path: '/dashboard' },
+        { id: 2, item: 'Alerts', Icon: alertIcon, path: '/alerts' },
+        { id: 3, item: 'Defend', Icon: defendIcon, path: '/defend' },
+        { id: 4, item: 'Protect', Icon: protectIcon, path: '/protect' },
+    ]
     return (
         <div className='flex w-1/5 flex-col h-[100vh] '>
             {/* <div className='flex mt-6 justify-center'>
                 <img src={logo2} alt="" />
             </div> */}
             <div className='flex flex-col justify-center items-center font-display font-medium text-base font-white mt-[55px]'>
-                <button className='flex flex-row w-[194px] h-[48px] rounded-lg justify-center items-center active:bg-blue-0 text-gray-dark-0 active:text-white-0 active:fill-white-0 active:stroke-white-0' >
-                    <div>
 
-                        <img src={dIcon} />
+                <div className='flex'>
+                    <div className='flex w-1/5 flex-col h-[100vh] mt-[39px]'>
+                        {items.map(({ Icon, ...item }) => {
+                            return (
+                                <div className='flex flex-col justify-center items-center font-display font-medium text-base font-white mt-[14px]' key={item.id}>
+                                    <Link className="nav-link" to={item.path}>
+                                        <button className='flex flex-row w-[194px] h-[48px] rounded-lg justify-center items-center active:bg-blue-0 text-gray-dark-0 active:text-white-0 active:fill-white-0 active:stroke-white-0'>
+                                            <div><Icon fill='icon-gray-0 ' /></div>
+                                            <label className='ml-3' >{item.item}</label>
+                                        </button>
+                                    </Link>
+                                </div>
+                            )
+                        })}
                     </div>
-                    <label className='ml-3 '>Dashboard</label>
-                </button>
-                <button className='flex flex-row w-[194px] h-[48px] rounded-lg justify-center items-center active:bg-blue-0 text-gray-dark-0 active:text-white-0 active:fill-white-0 active:stroke-white-0' >
-                    <div><img src={alertIcon} /> </div>
-                    <label className='ml-3 '>Alerts</label>
-                </button>
-                <button className='flex flex-row w-[194px] h-[48px] rounded-lg justify-center items-center active:bg-blue-0 text-gray-dark-0 active:text-white-0 active:fill-white-0 active:stroke-white-0' >
-                    <div><img src={defendIcon} /> </div>
-                    <label className='ml-3 '>Defend</label>
-                </button>
-                <button className='flex flex-row w-[194px] h-[48px] rounded-lg justify-center items-center active:bg-blue-0 text-gray-dark-0 active:text-white-0 active:fill-white-0 active:stroke-white-0' >
-                    <div><img src={protectIcon} /> </div>
-                    <label className='ml-3 '>Protect</label>
-                </button>
-
+                    {/* <Dashboard /> */}
+                </div>
 
 
 
